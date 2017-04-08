ae06da14c9031ded45657af8e8465d6ea1ee6c24
Guilherme Ventura <gventura@ciandt.com>
2017-04-08 12:00:34 -0300

    Better loggin database url

--- database/database.js
+++ database/database.js
@@ -9,7 +9,7 @@ const pass = 'GuRzZox9VN2oqzjX';
 const db = 'buzzer-dev';
 // Connection URL
 const url = process.env.MONGO_CONNECTION_STR || `mongodb://${user}:${pass}@cluster0-shard-00-00-gu8zv.mongodb.net:27017,cluster0-shard-00-01-gu8zv.mongodb.net:27017,cluster0-shard-00-02-gu8zv.mongodb.net:27017/${db}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
-console.log("URL", url);
+console.log("Database URL", url);
 
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise
